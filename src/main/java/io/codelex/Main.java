dd0293da71cdb1cc3610f60b3ab8828b8ed619e1
Zelchs <raivis0211@gmail.com>
2024-06-07 15:56:25 +0300

    Fix main method

--- src/main/java/io/codelex/Main.java
+++ src/main/java/io/codelex/Main.java
@@ -2,7 +2,7 @@ package io.codelex;
 
 public class Main {
     public static void main(String[] args) {
-        String iban = "GB82 WEST 1234 5698 7654 32";
+        String iban = args.length > 0 ? args[0] : "";
 
         if (IbanValidator.validate(iban)) {
             System.out.println("IBAN is valid");
